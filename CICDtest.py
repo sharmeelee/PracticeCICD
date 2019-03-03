5162be2d49ea8a2adfa4b14257d25328f4144ad6
sharmeelee <48166985+sharmeelee@users.noreply.github.com>
2019-03-03 14:23:00 -0800

    Update CICDtest.py
    edit logging for testing auto-build in Jenkins

--- CICDtest.py
+++ CICDtest.py
@@ -5,4 +5,4 @@ import logging
 
 logging.basicConfig(filename = 'C:/Users/SBijlan/Documents/Automation/DIY/PracticeCICD/cicdPractice.log', filemode = 'a', level = logging.INFO, format = '%(asctime)s  %(levelname)-10s %(message)s')
 
-logging.info("Practice run - edit via github")
+logging.info("Practice run - test auto-build in Jenkins based on changes in SCM")

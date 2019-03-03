37844d35c22d84d70d7d394c3ddeb432482bff49
sharmeelee <48166985+sharmeelee@users.noreply.github.com>
2019-03-02 17:32:42 -0800

    Delete logging.py

--- logging.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-
-logging.basicConfig(filename = 'C:/Users/SBijlan/Documents/Automation/DIY/PracticeCICD/cicdPractice.log', filemode = 'a', level = logging.INFO, format = '%(asctime)s  %(levelname)-10s %(message)s')
-
-logging.info("Practice run")

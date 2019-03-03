59eb39f9c6a44d6dff69eea8718f832862d9e8a2
sharmeelee <48166985+sharmeelee@users.noreply.github.com>
2019-03-02 17:32:10 -0800

    Add files via upload

---
+++ CICDtest.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import logging
+
+
+logging.basicConfig(filename = 'C:/Users/SBijlan/Documents/Automation/DIY/PracticeCICD/cicdPractice.log', filemode = 'a', level = logging.INFO, format = '%(asctime)s  %(levelname)-10s %(message)s')
+
+logging.info("Practice run")
